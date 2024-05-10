d5939a8aa713ee0d0021a499320d943d6969142e
Shubham Sharma <8696771553shubham@gmail.comm>
2024-05-10 22:42:41 +0530

    Created Main Component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,46 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Jarvis</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder="Enter a prompt here" />
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
+            </div>
+            <p className="bottom-info">
+              Gemini may display inaccurate info, including about people, so
+              double-check its responses. Your privacy and Gemini Apps
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
